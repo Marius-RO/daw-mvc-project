f66fafce87dbec62d29a3be7cefe28a84ed9a145
Marius-RO <blw_matt@rocketmail.com>
2020-12-26 14:26:07 +0200

    Adauga operatiile CRUD pentru controllerul Bike (#2)

--- BikeShop/Controllers/BikeController.cs
+++ BikeShop/Controllers/BikeController.cs
@@ -9,16 +9,17 @@ namespace BikeShop.Controllers
 {
     public class BikeController : Controller
     {
+        private ApplicationDbContext ctx = new ApplicationDbContext();
 
         public ActionResult Index()
         {
-            List<Bike> bikes = new List<Bike>();
+            List<Bike> bikes = ctx.Bikes.ToList();
             return View(bikes);
         }
 
         public ActionResult Details(int id)
         {
-            Bike bike = new Bike();
+            Bike bike = ctx.Bikes.Find(id);
             return View(bike);
         }
 
@@ -29,24 +30,44 @@ namespace BikeShop.Controllers
             return View(bike);
         }
 
+        [HttpPost]
         public ActionResult Create(Bike bike)
         {
+            ctx.Bikes.Add(bike);
+            ctx.SaveChanges();
             return RedirectToAction("Index");
         }
 
         public ActionResult Edit(int id)
         {
-            Bike bike = new Bike();
+            Bike bike = ctx.Bikes.Find(id);
             return View(bike);
         }
 
-        public ActionResult Update()
+        [HttpPost]
+        public ActionResult Update(Bike updatedBike)
         {
+
+            Bike bike = ctx.Bikes.Single(b => b.BikeId == updatedBike.BikeId);
+            bike.Name = updatedBike.Name;
+            bike.Description = updatedBike.Description;
+            bike.FabricationDate = updatedBike.FabricationDate;
+            bike.Price = updatedBike.Price;
+
+            ctx.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        public ActionResult Delete()
+
+        [HttpPost]
+        public ActionResult Delete(int id)
         {
+
+            Console.WriteLine("Aici");
+
+            Bike bike = ctx.Bikes.Find(id);
+            ctx.Bikes.Remove(bike);
+            ctx.SaveChanges();
             return RedirectToAction("Index");
         }
 
