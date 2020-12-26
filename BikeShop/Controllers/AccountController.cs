7c769233408d0344f1ee737670e02ae955608579
Marius-RO <blw_matt@rocketmail.com>
2020-12-26 10:35:23 +0200

    Crearea rolurilor pentru admin, seller si user (#5)

--- BikeShop/Controllers/AccountController.cs
+++ BikeShop/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using BikeShop.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace BikeShop.Controllers
 {
@@ -155,6 +156,14 @@ namespace BikeShop.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+
+                    var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+                    if (!roleManager.RoleExists("User"))
+                        roleManager.Create(new IdentityRole("User"));
+                    UserManager.AddToRole(user.Id, "User");
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
--- BikeShop/Startup.cs
+++ BikeShop/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using BikeShop.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(BikeShop.Startup))]
@@ -9,6 +12,50 @@ namespace BikeShop
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateAdminAndUserRoles();
+        }
+
+        private void CreateAdminAndUserRoles()
+        {
+            var ctx = new Models.ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(ctx));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(ctx));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "admin@admin.com";
+                user.Email = "admin@admin.com";
+
+                var adminCreated = userManager.Create(user, "admin1234");
+                if (adminCreated.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+
+            if (!roleManager.RoleExists("Seller"))
+            {
+          
+                var role = new IdentityRole();
+                role.Name = "Seller";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "seller@seller.com";
+                user.Email = "seller@seller.com";
+
+                var sellerCreated = userManager.Create(user, "seller1234");
+                if (sellerCreated.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Seller");
+                }
+            }
         }
     }
 }
