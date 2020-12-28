442636680a8dfae09b7c4214744221e70e7ad773
Marius-RO <blw_matt@rocketmail.com>
2020-12-28 14:17:13 +0200

    Adauga validari + modifica view-urile pentru Bike
    * Adauga validarile necesare campurilor din Bike (#1)
    
    * Adauga un nou model (CheckBox model) pentru a putea folosi un view
      pentru selectii many-to-many
    
    * Modifica view-urilor a.i. sa cuprinda validarile si CheckBox-urile
      pentru afisarea selectiei pieselor (#3)
    
    * Adauga preluarea piseleor selectate in controller atunci cand se
      creeaza o noua bicicleta

--- BikeShop/Controllers/BikeController.cs
+++ BikeShop/Controllers/BikeController.cs
@@ -27,15 +27,39 @@ namespace BikeShop.Controllers
         public ActionResult New()
         {
             Bike bike = new Bike();
+            bike.PiecesListCheckBoxes = GetAllPiecesChecboxes();
             return View(bike);
         }
 
         [HttpPost]
         public ActionResult Create(Bike bike)
         {
-            ctx.Bikes.Add(bike);
-            ctx.SaveChanges();
-            return RedirectToAction("Index");
+            if (!ModelState.IsValid)
+            {
+                return View("New", bike);
+            }
+
+            var selectedPieces = bike.PiecesListCheckBoxes.Where(b => b.Checked).ToList();
+
+            try
+            {
+                bike.Pieces = new List<Piece>();
+                for (int i = 0; i < selectedPieces.Count(); i++)
+                {
+                    Piece piece = ctx.Pieces.Find(selectedPieces[i].Id);
+                    bike.Pieces.Add(piece);
+                }
+
+                ctx.Bikes.Add(bike);
+                ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                var msg = e.Message;
+                Console.WriteLine(e.Message);
+                return View(bike);
+            }
         }
 
         public ActionResult Edit(int id)
@@ -47,12 +71,17 @@ namespace BikeShop.Controllers
         [HttpPost]
         public ActionResult Update(Bike updatedBike)
         {
+            if (!ModelState.IsValid)
+            {
+                return View("Edit", updatedBike);
+            }
 
             Bike bike = ctx.Bikes.Single(b => b.BikeId == updatedBike.BikeId);
             bike.Name = updatedBike.Name;
             bike.Description = updatedBike.Description;
             bike.FabricationDate = updatedBike.FabricationDate;
             bike.Price = updatedBike.Price;
+            bike.Quantity = updatedBike.Quantity;
 
             ctx.SaveChanges();
             return RedirectToAction("Index");
@@ -63,14 +92,28 @@ namespace BikeShop.Controllers
         public ActionResult Delete(int id)
         {
 
-            Console.WriteLine("Aici");
-
             Bike bike = ctx.Bikes.Find(id);
             ctx.Bikes.Remove(bike);
             ctx.SaveChanges();
             return RedirectToAction("Index");
         }
 
+        [NonAction]
+        private List<CheckBoxModel> GetAllPiecesChecboxes()
+        {
+            var checkboxList = new List<CheckBoxModel>();
+            foreach (var piece in ctx.Pieces.ToList())
+            {
+                checkboxList.Add(new CheckBoxModel
+                {
+                    Id = piece.PieceId,
+                    Name = piece.Name,
+                    DisplayName = "  " + piece.Name,
+                    Checked = false
+                });
+            }
+            return checkboxList;
+        }
 
     }
 }
--- BikeShop/Models/Bike.cs
+++ BikeShop/Models/Bike.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BikeShop.Models
 {
@@ -8,16 +9,36 @@ namespace BikeShop.Models
     {
         [Key]
         public int BikeId { get; set; }
-        public float Price { get; set; }
+
+        [Display(Name = "Nume")]
+        [Required(ErrorMessage = "Numele nu a fost introdus")]
+        [MaxLength(20, ErrorMessage = "Numele nu poate fi mai mare 20 de caractere")]
         public string Name { get; set; }
+
+        [Display(Name = "Descriere")]
+        [Required(ErrorMessage = "Descrierea nu a fost introdusa")]
+        [MaxLength(350, ErrorMessage = "Descrierea nu poate fi mai mare 350 de caractere")]
         public string Description { get; set; }
+
+        [Display(Name = "Data fabricatie")]
+        [Required(ErrorMessage = "Data fabricatiei nu a fost selectata")]
         public DateTime FabricationDate { get; set; }
+
+        [Display(Name = "Cantitate")]
+        [Range(1,100, ErrorMessage = "Cantitate incorecta (1,100)")]
         public int Quantity { get; set; }
+
+        [Display(Name = "Pret")]
+        [Range(1, 1000, ErrorMessage = "Pret incorect (1,1000)")]
+        public float Price { get; set; }
 
         public virtual ICollection<Piece> Pieces { get; set; }
         public virtual ICollection<Accessory> Accessories { get; set; }
 
         public int UserId { get; set; }
         public virtual ApplicationUser User { get; set; }
+
+        [NotMapped]
+        public List<CheckBoxModel> PiecesListCheckBoxes { get; set; }
     }
 }
