0bdc3000c7e23c28b3bfe8010d733ad094fe9226
Marius-RO <blw_matt@rocketmail.com>
2021-01-04 18:36:14 +0200

    Schimba modul de afisare al unor informatii din view-uri

--- BikeShop/Controllers/HomeController.cs
+++ BikeShop/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BikeShop.Config;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,8 @@ namespace BikeShop.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            //return View();
+            return RedirectToAction(Utilities.ACTION_INDEX, Utilities.BIKE_CONTROLLER);
         }
 
         public ActionResult About()
